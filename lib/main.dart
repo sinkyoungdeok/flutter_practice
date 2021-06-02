e31107b5c8613e27c15ce1b738c2bc5622886cd9
singyeongdeog <tlsrudejr123@nate.com>
2021-06-02 23:36:52 +0900

    [ADD] (완전초보용) 9

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-      home: MyHomePage(),
+      title: 'Character card',
+      home: MyCard(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First app'),
+        title: Text('BBANTO'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0, // 앱바를 앱에서 뜨는 효과를 제거하거나 키울 수 있다
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello')
+            Text('Hell'),
+            Text('Hell'),
+            Text('Hell'),
           ],
         ),
-      ),
+      )
     );
   }
 }
