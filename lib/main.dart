7a2ad931c48a2ef8abdf6f520d40daeadcb492a5
sinkyoungdeok <tlsrudejr123@nate.com>
2021-06-05 18:21:23 +0900

    [ADD] (완전초보용) 15

--- lib/main.dart
+++ lib/main.dart
@@ -23,12 +23,6 @@ class MyPage extends StatelessWidget {
         title: Text('Appbar icon menu'),
         centerTitle: true,
         elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('menu button is clicked');
-          },
-        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.shopping_cart),
@@ -43,6 +37,31 @@ class MyPage extends StatelessWidget {
             },
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/chef.gif'),
+                backgroundColor: Colors.white,
+              ),
+              accountName: Text('SGD'),
+              accountEmail: Text('tkagmd1@naver.com'),
+              onDetailsPressed: (){
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
