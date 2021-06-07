1751e9468b8bceb9cffd7441b3d38df848357264
singyeongdeog <tlsrudejr123@nate.com>
2021-06-07 21:33:37 +0900

    [ADD] (완전 초보용) 19 snack bar v2

--- lib/main.dart
+++ lib/main.dart
@@ -23,25 +23,30 @@ class MyPage extends StatelessWidget {
         title: Text('Snack Bar'),
         centerTitle: true,
       ),
-      body: Builder(
-        builder: (BuildContext ctx) {
-          return Center(
-            child: FlatButton(
-              child: Text('Show me',
-                style: TextStyle(
-                    color: Colors.white
-                ),
-              ),
-              color: Colors.red,
-              onPressed: () {
-                Scaffold.of(ctx).showSnackBar(SnackBar(
-                  content : Text('Hello'),
-                ));
-              },
-            ),
-          );
-      },)
+      body: MySnackBar(),
     );
   }
 }
 
+class MySnackBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('Show me'),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(SnackBar(content: Text('hello',
+          textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+            backgroundColor: Colors.teal,
+            duration: Duration(milliseconds: 1000),
+          ),
+          );
+        }
+      ),
+    );
+  }
+}
