8688287ff077caec61dbe50c011d957417386e1f
singyeongdeog <tlsrudejr123@nate.com>
2021-06-07 21:44:36 +0900

    [ADD] (완전 초보용) 19 toastMessage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Snack Bar',
+      title: 'Toast message',
       theme: ThemeData(
         primarySwatch: Colors.red
       ),
@@ -20,33 +21,29 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Snack Bar'),
+        title: Text('Toast message'),
         centerTitle: true,
       ),
-      body: MySnackBar(),
+      body: Center(
+        child: FlatButton(
+          onPressed: () {
+            flutterToast();
+          },
+          child: Text('Toast'),
+          color: Colors.blue,
+        ),
+      ),
     );
   }
 }
 
-class MySnackBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        child: Text('Show me'),
-        onPressed: () {
-          Scaffold.of(context).showSnackBar(SnackBar(content: Text('hello',
-          textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white
-            ),
-          ),
-            backgroundColor: Colors.teal,
-            duration: Duration(milliseconds: 1000),
-          ),
-          );
-        }
-      ),
-    );
-  }
+
+void flutterToast() {
+  Fluttertoast.showToast(msg: 'Flutter',
+  gravity: ToastGravity.BOTTOM,
+    backgroundColor: Colors.redAccent,
+    fontSize: 20.0,
+    textColor: Colors.white,
+    toastLength: Toast.LENGTH_SHORT
+  );
 }
