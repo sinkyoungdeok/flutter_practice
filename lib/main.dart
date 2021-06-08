24e979ef2190d1979fc2886b3f4642da59b1c9f2
sinkyoungdeok <tlsrudejr123@nate.com>
2021-06-08 12:55:16 +0900

    [ADD] (완전 초보용) 21 column, row

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Container',
-      theme: ThemeData(
-        primarySwatch: Colors.red
-      ),
+      theme: ThemeData(primarySwatch: Colors.red),
       home: MyPage(),
     );
   }
 }
+
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.green,
       body: SafeArea(
-        child: Container(
-          color: Colors.red,
-          width: 100,
-          height: 100,
-          margin: EdgeInsets.symmetric(
-            vertical: 50,
-            horizontal: 10
-          ),
-          padding: EdgeInsets.all(40),
-          child: Text('Hello'),
+        child: Row(
+          //mainAxisAlignment: MainAxisAlignment.center, // 얘는 세로축을 전체를 가지고 있음
+          //mainAxisSize: MainAxisSize.min, // 얘는 세로축을 children만큼만 가짐
+          //verticalDirection: VerticalDirection.up, // 얘는 밑에서부터 위로 widget을 쌓는식으로 바꿈
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly, // widget만큼 space를 동일하게 줌
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            SizedBox(
+              width: 30.0,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Text('Container 3'),
+            ),
+            Container(
+              width: double.infinity,
+              height: 20,
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
